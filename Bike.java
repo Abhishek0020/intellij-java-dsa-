87eed26af0277172abde582c7495fe98f3debb2c
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2023-03-28 17:17:44 +0530

    java+dsa

---
+++ Bike.java
@@ -0,0 +1,15 @@
+//abstract class Bike {
+//    abstract void run();
+//
+//    class honda extends Bike{
+//        void run() {
+//            System.out.println("bike is running : ");
+//        }
+//
+//        public  void main(String[] args) {
+//            Bike b=new honda();
+//            b.run();
+//        }
+//    }
+//
+//}
---
+++ do_while_loop.java
@@ -0,0 +1,14 @@
+//import java.util.Scanner;
+//
+//public class do_while_loop {
+//    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter the number : ");
+//        int n= sc.nextInt();
+//        int i=1;
+//        do{
+//            System.out.println(2*i);
+//            i++;
+//        }while (i<=n);
+//    }
+//}
