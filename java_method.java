2aec0f06ae909224fc46af091cb20ef61ac5a190
Abhishek0020 <90542072+Abhishek0020@users.noreply.github.com>
2023-03-28 17:23:16 +0530

    Add files via upload

--- java_method.java
+++ java_method.java
@@ -1,16 +1,9 @@
-//import java.util.Scanner;
-//
 //public class java_method {
-//    public static void multipleTable(int n){
-//        Scanner sc=new Scanner(System.in);
-//        System.out.println("enter the number : ");
-//        n= sc.nextInt();
-//        for (int i = 1; i <=10; i++) {
-//            System.out.println(2*i);
-//        }
+//    static void myMethod(String f_name){
+//        System.out.println("doe"+f_name);
 //    }
 //
 //    public static void main(String[] args) {
-//        multipleTable(10);
+//        myMethod("john");
 //    }
 //}
